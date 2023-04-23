12926d2c7948411022be248033033e08b2fc9872
HELDERK1 <helderk2011@hotmail.com>
2023-04-23 18:48:21 -0300

    Adicionado regras e layout

--- src/desafiofinalventurus/FXMLDocumentController.java
+++ src/desafiofinalventurus/FXMLDocumentController.java
@@ -7,9 +7,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -18,6 +20,7 @@ import javafx.scene.layout.StackPane;
 public class FXMLDocumentController implements Initializable {
 
     boolean x = true;
+    boolean btEscolha = false;
     String url = "";
     boolean dadosInseridos[][] = new boolean[3][3];
     String figurasInseridos[][] = new String[3][3];
@@ -26,7 +29,33 @@ public class FXMLDocumentController implements Initializable {
     RegraGame objRegra = new RegraGame();
 
     @FXML
-    private ImageView im_1X1, im_1X2, im_1X3, im_2X1, im_2X2, im_2X3, im_3X1, im_3X2, im_3X3, im_win;
+    private ImageView im_1X1, im_1X2, im_1X3, im_2X1, im_2X2, im_2X3, im_3X1, im_3X2, im_3X3, im_win, win;
+    @FXML
+    AnchorPane TelaGame;
+    @FXML
+    Button bt_1X1, bt_1X2, bt_1X3, bt_2X1, bt_2X2, bt_2X3, bt_3X1, bt_3X2, bt_3X3;
+    @FXML
+    Label lbEscolha, lbResult;
+
+    @FXML
+    private void BT_EscolhaO(ActionEvent event) {
+        if (!btEscolha) {
+            x = false;
+            lbEscolha.setText("O - foi excolhido");
+            btEscolha = true;
+            AtivaDesativaBotoes(false);
+        }
+    }
+
+    @FXML
+    private void BT_EscolhaX(ActionEvent event) {
+        if (!btEscolha) {
+            x = true;
+            lbEscolha.setText("X - foi excolhido");
+            btEscolha = true;
+            AtivaDesativaBotoes(false);
+        }
+    }
 
     @FXML
     private void BT_1x1(ActionEvent event) {
@@ -150,13 +179,15 @@ public class FXMLDocumentController implements Initializable {
             if (objRegra.ValidaGame()) {
                 AdicionaBarraWin();
                 System.out.println("Win");
+            } else {
+                ValidaVelha();
             }
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        AtivaDesativaBotoes(true);
     }
 
     private Image RetornaImage() {
@@ -169,6 +200,12 @@ public class FXMLDocumentController implements Initializable {
         String urlWin = objRegra.getPosWin();
         Image image = new Image(getClass().getResourceAsStream(urlWin));
         im_win.setImage(image);
+        AtivaDesativaBotoes(true);
+
+        url = objInsere.RetornaFigura(!x);
+        Image imageWin = new Image(getClass().getResourceAsStream(url));
+        win.setImage(imageWin);
+        lbResult.setText("Ganhou!");
     }
 
     private void AcoesBotao(int i, int j) {
@@ -186,5 +223,24 @@ public class FXMLDocumentController implements Initializable {
         } else {
             System.out.println("Botao já clicado");
         }
+    }
+
+    private void AtivaDesativaBotoes(Boolean active) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                dadosInseridos[i][j] = active;
+            }
+        }
+    }
+
+    private void ValidaVelha() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (!dadosInseridos[i][j]) {
+                    return;
+                }
+            }
+        }
+        lbResult.setText("Empate!");
     }
 }
